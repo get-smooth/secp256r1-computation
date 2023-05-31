6561b4a9f0215598669a136bda9820f5b36cb7c1
qd-qd <qdqdqdqdqd@protonmail.com>
2023-05-31 16:57:01 +0200

    ✅ unit-test `index.ts`
    This is main file expected to be used by the consumer of the library.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import { secp256r1 } from "@noble/curves/p256";
 import { concatenatePoints, computePoints } from "./utils";
 
-export default async function getPrecomputedPoints() {
-  // Load the C0 and C1 environment variables and convert them to BigInts
-  const C0 = BigInt(process.env.C0);
-  const C1 = BigInt(process.env.C1);
-
-  // Get the public key using C0 and C1
-  const Q = secp256r1.ProjectivePoint.fromAffine({ x: C0, y: C1 });
+export default async function precomputePoints(x: bigint, y: bigint): Promise<string> {
+  // Get the public key using x and y
+  const pubKey = secp256r1.ProjectivePoint.fromAffine({ x, y });
 
   // Precompute the points associated to the public key
-  const precomputedPoints = computePoints(Q);
+  const precomputedPoints = computePoints(pubKey);
 
   // Concatenate the points into a single string
-  const concatenated_points = concatenatePoints(precomputedPoints);
+  const concatenatedPoints = concatenatePoints(precomputedPoints);
 
-  return concatenated_points;
+  return concatenatedPoints;
 };
---
+++ tests/index.test.ts
@@ -0,0 +1,47 @@
+import precomputePoints from "../src";
+import { secp256r1 } from "@noble/curves/p256";
+import isPointOnCurve from "./utils/isPointOnCurve";
+
+/**
+ * Splits a concatenated string of x and y coordinates into an array of {x, y} points.
+ * @param concatenedPoints The concatenated string of x and y coordinates.
+ * @returns An array of {x, y} points.
+ */
+function chunkPrecomputedPoints(concatenedPoints: string): { x: bigint, y: bigint }[] {
+    const points: { x: bigint, y: bigint }[] = [];
+
+    for (let i = 0; i < concatenedPoints.length; i += 256) {
+        const concatenateCoords = concatenedPoints.slice(i, i + 128);
+        const [x, y] = [concatenateCoords.slice(0, 64), concatenateCoords.slice(64)];
+
+        points.push({ x: BigInt(`0x${x}`), y: BigInt(`0x${y}`) });
+    }
+
+    return points;
+}
+
+describe("specific case", () => {
+    it("the precompute points contain has the correct length", async () => {
+        const priv = secp256r1.utils.randomPrivateKey();
+        const pubKey = secp256r1.ProjectivePoint.fromPrivateKey(priv);
+        const pointsStringify = await precomputePoints(pubKey.x, pubKey.y);
+
+        // x and y are 32 bytes long, so 64 characters in the string representation
+        // we generate 256 points, each point is the concatenation of x and y, so
+        // 64 * 2 = 128 characters per point. We have 256 points, so 128 * 256 characters
+        // in the string representation = 32768
+        expect(pointsStringify.length).toEqual(64 * 2 * 256);
+    });
+
+    it("all precomputed points are on the curve", async () => {
+        const priv = secp256r1.utils.randomPrivateKey();
+        const pubKey = secp256r1.ProjectivePoint.fromPrivateKey(priv);
+        const pointsStringify = await precomputePoints(pubKey.x, pubKey.y);
+
+        // the first point (the point zero in our implementation) is not used
+        const [, ...points] = chunkPrecomputedPoints(pointsStringify);
+
+        points.forEach(({ x, y }) => expect(isPointOnCurve(x, y)).toBeTruthy());
+    });
+});
+
